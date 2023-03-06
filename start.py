5c6caaecddc486775ef416a372e291189e5036a7
Rodrigo Duran <rduran@chapman.edu>
2023-03-06 13:44:57 -0800

    starting

---
+++ start.py
@@ -0,0 +1,15 @@
+#create a csv file
+
+#get user input
+    #name, email, address
+    #symptons
+    #allergies ?
+
+
+
+
+
+                                        #BACKEND
+#assign minerals/nontropics to symptons/diseases
+
+
